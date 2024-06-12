6ce4eb458e2e7bc8a71f9f031580213f828a4386
Tamuna Kakhidze <tamuna.kakhidze.1@btu.edu.ge>
2024-06-12 15:20:55 +0400

    add environment object to contentview

--- WeatherApp/App/WeatherAppApp.swift
+++ WeatherApp/App/WeatherAppApp.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
-
+ 
 @main
 struct WeatherAppApp: App {
+    @StateObject private var viewModel = WeatherViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
         }
     }
 }
